e25b8851ef7d3c32a9fc1ac692e9927c4e65ae87
Brian C. Milco <bcmilco@gmail.com>
2020-05-23 22:10:51 -0700

    Allow the netbios domain to be configured separately.

--- init.sh
+++ init.sh
@@ -17,7 +17,7 @@ appSetup () {
 	
 	LDOMAIN=${DOMAIN,,}
 	UDOMAIN=${DOMAIN^^}
-	URDOMAIN=${UDOMAIN%%.*}
+	URDOMAIN=${NETBIOS}
 
 	# If multi-site, we need to connect to the VPN before joining the domain
 	if [[ ${MULTISITE,,} == "true" ]]; then
